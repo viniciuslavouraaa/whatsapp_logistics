36dde97fbf6d68a2e01eae36b459afa14fa56fba
Vini Lavoura <viniciuslavoura.dev@gmail.com>
2025-04-25 19:12:14 -0300

    Corrigido erro do código ir direto pra segunda parte

--- chatbot/clientFlow.py
+++ chatbot/clientFlow.py
@@ -11,11 +11,21 @@ if st.session_state['flow_step'] == 'initial_choice':
         'Você está:',
         ('Buscando cargas', 'Buscando motoristas parceiros')
     )
+    if st.button('Confirmar'):
+        if user_type == 'Buscando motoristas parceiros':
+            st.session_state['user_type'] = 'empresa'
+            st.session_state['flow_step'] = 'empresa_info' # Próximo passo para empresa
+        elif user_type == 'Buscando cargas':
+            st.session_state['user_type'] = 'caminhoneiro'
+            st.session_state['flow_step'] = 'motorista_info' # Próximo passo para caminhoneiro
+        st.rerun()   
+        
+elif st.session_state['flow_step'] == 'empresa_info':
+    st.write('Estamos felizes que você escolheu a Lavoura Transporte para cuidar da sua carga!')
+    st.write('Aqui vamos coletar algumas informações da empresa')
+    # Adicione aqui os campos para coletar informações da empresa
 
-    if user_type == 'Buscando motoristas parceiros':
-        st.session_state['user_type'] = 'empresa'
-        st.session_state['flow_step'] = 'empresa_info' # Próximo passo para empresa
-             # Força o Streamlit a executar o script novamente para exibir a próxima etapa
-    elif user_type == 'Buscando cargas':
-        st.session_state['user_type'] = 'caminhoneiro'
-        st.session_state['flow_step'] = 'motorista_info' # Próximo passo para caminhoneiro
+elif st.session_state['flow_step'] == 'motorista_info':
+    st.write('Estamos felizes que você escolheu a Lavoura Transporte para buscar cargas')
+    st.write('Aqui vamos coletar algumas informações da empresa')
+    # Adicione aqui os campos para coletar informações do caminhoneiro
